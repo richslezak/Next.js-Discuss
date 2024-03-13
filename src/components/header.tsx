e6af97467aa2f4abd04c34d20ddacecccb8dbacc
richslezak <richslezak@gmail.com>
2024-03-13 14:27:18 -0400

    fixed cache issue and began topic form

---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Input } from '@nextui-org/react';
+import React from 'react';
+import HeaderAuth from './header-auth';
+
+export default function Header() {
+  return (
+    <Navbar className="shadow mb-6">
+      <NavbarBrand>
+        <Link href="/" className="font-bold">
+          Discuss
+        </Link>
+      </NavbarBrand>
+      <NavbarContent justify="center">
+        <NavbarItem>
+          <Input placeholder="search" />
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <HeaderAuth />
+      </NavbarContent>
+    </Navbar>
+  );
+}
