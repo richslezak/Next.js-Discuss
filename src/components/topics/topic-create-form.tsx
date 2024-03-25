89c8a5d00a4d2aef6c32b44cd1e74e7fecf229d5
richslezak <richslezak@gmail.com>
2024-03-25 19:21:40 -0400

    added useFormState hook and validation

--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useFormState } from 'react-dom';
 import {
   Input,
   Button,
@@ -9,17 +12,40 @@ import {
 import * as actions from '@/actions';
 
 export default function TopicCreateForm() {
+  const [formState, action] = useFormState(actions.createTopic, {
+    errors: {},
+  });
+
   return (
     <Popover placement="left">
       <PopoverTrigger>
         <Button color="primary">Create a Topic</Button>
       </PopoverTrigger>
       <PopoverContent>
-        <form action={actions.createTopic}>
+        <form action={action}>
           <div className="flex flex-col gap-4 p-4 w-80">
             <h3 className="text-lg">Create a Topic</h3>
-            <Input label="Name" labelPlacement="outside" placeholder="Name" />
-            <Textarea label="Description" labelPlacement="outside" placeholder="Description" />
+            <Input
+              name="name"
+              label="Name"
+              labelPlacement="outside"
+              placeholder="Name"
+              isInvalid={!!formState.errors.name}
+              errorMessage={formState.errors.name?.join(', ')}
+            />
+            <Textarea
+              name="description"
+              label="Description"
+              labelPlacement="outside"
+              placeholder="Description"
+              isInvalid={!!formState.errors.description}
+              errorMessage={formState.errors.description?.join(', ')}
+            />
+            {formState.errors._form ? (
+              <div className="p-2 bg-red-200 border border-red-400 rounded-xl text-red-500">
+                {formState.errors._form.join(', ')}
+              </div>
+            ) : null}
             <Button type="submit">Submit</Button>
           </div>
         </form>
