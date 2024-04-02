ae2c04052f3820d50d03c215f7c4cf2887e55f2e
richslezak <richslezak@gmail.com>
2024-04-01 20:01:14 -0400

    updated prisma client to stop generating instances

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from '@prisma/client';
 
-export const db = new PrismaClient();
+let db: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+  db = new PrismaClient();
+} else {
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  };
+  if (!globalWithPrisma.prisma) {
+    globalWithPrisma.prisma = new PrismaClient();
+  }
+  db = globalWithPrisma.prisma;
+}
+
+export { db };
